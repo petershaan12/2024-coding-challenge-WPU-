c8640dc758ead9048b18899457a51d1fb8768741
petershaan12 <peterhiku12@gmail.com>
2024-01-29 14:38:41 +0700

    day #3

---
+++ 3-hero.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024 by Peter Shaan
+// 3/365
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+// function hero(bullets, dragons) {
+//     return bullets >= 2 * dragons;
+// }
+
+const hero = (bullets, dragons) => bullets >= 2 * dragons
+
+console.log(hero(10, 2))
