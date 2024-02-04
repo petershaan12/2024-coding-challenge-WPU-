649677d45346885be1a75d0ec868ff3b0b6717a4
petershaan12 <2181002@unai.edu>
2024-02-04 22:45:23 +0700

    day #10

---
+++ 10-splitArray.js
@@ -0,0 +1,7 @@
+// WPU Coding Challenge 2024 by Peter Shaan
+// 10/365
+// https://www.codewars.com/kata/577a98a6ae28071780000989
+
+function stringToArray(string) {
+    return string.split(' ');
+}
