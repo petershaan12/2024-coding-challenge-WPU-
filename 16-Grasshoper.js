12e7700a1c00912b765b06c90113316c43c2ddd5
petershaan12 <2181002@unai.edu>
2024-02-09 15:06:00 +0700

    day #16

---
+++ 16-Grasshoper.js
@@ -0,0 +1,7 @@
+// WPU Coding Challenge 2024 by Peter Shaan
+// 16/365
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+const greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest'
+
+console.log(greet('Daniel', 'Daniel'))
