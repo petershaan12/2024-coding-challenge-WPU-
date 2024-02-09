a07da383d44ff1c58478e5c0080c3398216261fa
petershaan12 <2181002@unai.edu>
2024-02-09 15:16:05 +0700

    day #17

---
+++ 17-areaofparameters.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024 by Peter Shaan
+// 17/366
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab
+
+// const areaOrPerimeter = function(l, w) {
+//     return l == w ? l * w : 2 * (l + w)
+// };
+
+const areaOrPerimeter = (l, w) => l == w ? l * w : 2 * (l + w)
+
+console.log(areaOrPerimeter(3, 3))
