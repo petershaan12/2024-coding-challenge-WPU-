5732666a64989a9c52910eba13624c4fc52b1bee
petershaan12 <peterhiku12@gmail.com>
2024-03-17 16:37:23 +0700

    day #31

---
+++ 31-sumMix.js
@@ -0,0 +1,15 @@
+// WPU Coding Challenge 2024 by Peter Shaan
+// 31/365
+// https://www.codewars.com/kata/57eaeb9578748ff92a000009/
+
+// function sumMix(x) {
+//     let total = 0;
+//     for (let i = 0; i < x.length; i++) {
+//         total += +x[i]
+//     }
+//     return total
+// }
+
+const sumMix = (x) => x.reduce((acc, curr) => acc + +curr, 0)
+
+console.log(sumMix([9, 3, '7', '3']))
