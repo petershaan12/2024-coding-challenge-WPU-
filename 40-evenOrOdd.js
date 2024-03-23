6126157e5527a927f0a25884c7657e78c9f65fdd
petershaan12 <2181002@unai.edu>
2024-03-23 23:57:12 +0700

    day #40

---
+++ 40-evenOrOdd.js
@@ -0,0 +1,9 @@
+// WPU Coding Challenge 2024 by Peter Shaan
+// 40/365
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
+
+function evenOrOdd(number) {
+    return number % 2 ? 'Odd' : "Even"
+}
+
+console.log(evenOrOdd(2))
